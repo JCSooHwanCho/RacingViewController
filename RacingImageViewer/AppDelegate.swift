03587184bde7e1fd3df2d2fef5bb719dbcde0dd7
SooHwanCho <chh0107@gmail.com>
2019-10-01 17:05:37 +0900

    delete SceneDelegate to support iOS13 lower

--- RacingImageViewer/AppDelegate.swift
+++ RacingImageViewer/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  RacingImageViewer
+//  BoostCourseViewTransition
 //
-//  Created by 조수환 on 2019/09/25.
+//  Created by 조수환 on 23/04/2019.
 //  Copyright © 2019 조수환. All rights reserved.
 //
 
@@ -11,23 +11,35 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    func applicationWillResignActive(_ application: UIApplication) {
+        let operationCache = ImageOperationCache.shared
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        operationCache.cancelAllOperations()
+        operationCache.clearCache()
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
     }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
 
 }
+
